a5795c2e945efed1db505669f542360cbea53e95
Alex <aivaylovivanov@gmail.com>
2025-03-26 19:55:03 +0000

    Feat: add navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react'; 
+import Link from 'next/link';
+import { RiNextjsFill } from 'react-icons/ri';
 
 const Navbar = () => { 
     return (
-        <div>Navbar</div> 
+        <nav className="z-10 top-14 left-1/2 -translate-x-1/2 fixed flex items-center 
+        gap-5 py-2 px-4 rounded-full bg-black/60 backdrop-blur-md border text-white border-zinc-800">
+            <a href="">
+                <RiNextjsFill className="text-4xl"></RiNextjsFill>
+            </a>
+
+            <Link href="/" className="hover:text-gray-300 transition-colors">
+            <span>Home</span>
+            </Link>
+
+            <Link href="/" className="hover:text-gray-300 transition-colors">
+            <span>Manifesto</span>
+            </Link>
+
+        </nav>
     )
 }
 
