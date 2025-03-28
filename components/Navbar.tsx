fb9c840575e088be8d2570b15ae1d8db5a2770aa
Alex <aivaylovivanov@gmail.com>
2025-03-28 23:31:08 +0000

    Style: increase size of navbar and reposition

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,20 +4,21 @@ import { RiNextjsFill } from 'react-icons/ri';
 
 const Navbar = () => { 
     return (
-        <nav className="z-10 top-14 left-1/2 -translate-x-1/2 fixed flex items-center 
-        gap-5 py-2 px-4 rounded-full bg-black/60 backdrop-blur-md border text-white border-zinc-800">
-            <a href="">
+        <nav className="z-10 top-4 left-1/2 -translate-x-1/2 fixed flex items-center 
+        gap-6 px-6 py-3 rounded-full bg-black/60 backdrop-blur-md border text-white border-zinc-800 w-auto">
+            <a href="" className="flex items-center">
                 <RiNextjsFill className="text-4xl"></RiNextjsFill>
             </a>
 
-            <Link href="/" className="hover:text-gray-300 transition-colors">
-            <span>Home</span>
-            </Link>
+            <div className="flex items-center gap-6">
+                <Link href="/" className="hover:text-gray-300 transition-colors text-base font-medium py-1.5">
+                    <span>Home</span>
+                </Link>
 
-            <Link href="/" className="hover:text-gray-300 transition-colors">
-            <span>Manifesto</span>
-            </Link>
-
+                <Link href="/" className="hover:text-gray-300 transition-colors text-base font-medium py-1.5">
+                    <span>Manifesto</span>
+                </Link>
+            </div>
         </nav>
     )
 }
